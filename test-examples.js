469c38c596720f468b50611101ec424d80aa78cd
Andy Chou <acchou2@gmail.com>
2019-05-18 08:11:57 -0700

    make test-examples script install a local faastjs package for testing, if specified.

--- test-examples.js
+++ test-examples.js
@@ -1,51 +1,67 @@
 "use strict";
 
 const { readdirSync, statSync } = require("fs");
+const { resolve } = require("path");
 const { exec } = require("child_process");
 
 async function execCmd(cmd, options) {
     await new Promise((resolve, reject) =>
         exec(cmd, options, (err, stdout, stderr) => {
             if (err) {
+                console.log(stdout);
+                if (stderr) {
+                    console.log(stderr);
+                }
                 reject(err);
-            }
-            console.log(stdout);
-            if (stderr) {
-                console.log(stderr);
             }
             resolve();
         })
     );
 }
 
-async function process(test) {
-    let lang = "js";
-    if (test.match(/-ts$/)) {
-        lang = "ts";
+async function runTest(test, pkg) {
+    try {
+        let lang = "js";
+        if (test.match(/-ts$/)) {
+            lang = "ts";
+        }
+        const entries = readdirSync(test);
+        if (entries.find(file => file === "tsconfig.json")) {
+            lang = "ts";
+        }
+        await execCmd("npm install", { cwd: test });
+        if (pkg) {
+            await execCmd(`npm install ${pkg}`, { cwd: test });
+        }
+        if (lang === "ts") {
+            await execCmd("npm run build", { cwd: test });
+        }
+        await execCmd("npm run test", { cwd: test });
+        console.log(`[pass] ${test}`);
+    } catch (err) {
+        console.log(`[fail] ${test}: ${err}`);
+        throw err;
     }
-    const entries = readdirSync(test);
-    if (entries.find(file => file === "tsconfig.json")) {
-        lang = "ts";
-    }
-    await execCmd("npm install", { cwd: test });
-    if (lang === "ts") {
-        await execCmd("npm run build", { cwd: test });
-    }
-    await execCmd("npm run test", { cwd: test });
 }
 
-async function main() {
+async function main(pkg) {
     const entries = readdirSync(".").filter(
         entry =>
             entry !== "aws-top-packages" &&
             entry[0] !== "." &&
-            statSync(entry).isDirectory()
+            statSync(entry).isDirectory() &&
+            entry === "aws-ts"
     );
     const promises = entries.map(dir => {
         console.log(`testing ${dir}`);
-        return process(dir);
+        return runTest(dir, pkg);
     });
     await Promise.all(promises);
 }
 
-main();
+let pkg;
+if (process.argv[2]) {
+    pkg = resolve(process.argv[2]);
+    console.log(`resolved ${pkg}`);
+}
+main(pkg);
