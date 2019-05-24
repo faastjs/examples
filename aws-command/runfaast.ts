71b52c881317d4682057880790f7ab38fbcfde6a
Andy Chou <acchou2@gmail.com>
2019-05-23 22:54:19 -0700

    Fix example

--- aws-command/runfaast.ts
+++ aws-command/runfaast.ts
@@ -65,7 +65,7 @@ Options:
     const m = await faast(provider, funcs, {
         memorySize: 1728,
         timeout: 600,
-        include: [resolvedDir]
+        include: [resolvedDir!]
     });
     console.log(`Logs: ${m.logUrl()}`);
     try {
